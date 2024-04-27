e6e755a62b4d8faf39f8326b25c1552802da316f
B-e-a-t-r-i-z <beatriz.almeida2001@outlook.com>
2024-04-27 20:15:36 -0300

    primeiro commit

---
+++ javascript/script.js
@@ -0,0 +1,15 @@
+let btnMenu = document.getElementById('btn__abrir__menu');
+let menu = document.getElementById('menu__mobile');
+let overlay = document.getElementById('overlay__menu');
+
+btnMenu.addEventListener('click', () => {
+    menu.classList.add('abrir__menu')
+})
+
+menu.addEventListener('click', () => {
+    menu.classList.remove('abrir__menu')
+})
+
+overlay.addEventListener('click', () => {
+    menu.classList.remove('abrir__menu')
+})
